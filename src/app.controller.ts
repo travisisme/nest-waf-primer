5179ca745f24687fb041399262700af7b86624dc
Travis <travisok2003@gmail.com>
2022-02-20 14:00:44 +0100

    umm before ORM

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -9,4 +9,22 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('hello2')
+  @Render('index.html')
+  getHello2(): {} {
+    return this.appService.getHello2();
+  }
+
+  @Get()
+    @Render('home.html')
+    getHome(): {} {
+      return this.appService.getHome();
+    }
+
+  @Get('about-us')
+    @Render('about-us.html')
+    getAboutUs(): {} {
+      return this.appService.getAboutUs();
+    }
 }
