0056faec5c29c8cb0a5374d8208d2d117e6ee270
Travis <travisok2003@gmail.com>
2022-02-22 17:43:01 +0100

    oh god

---
+++ src/student-registration/users/entities/user.entity.ts
@@ -0,0 +1,31 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+    
+    @Column()
+    firstName: string;
+    
+    @Column({nullable: true})
+    middleName: string;
+    
+    @Column()
+    lastName: string;
+    
+    @Column()
+    email: string;
+    
+    @Column({nullable: true})
+    dateOfBirth: Date;
+    
+    @Column({nullable: true})
+    nationality: string
+    
+    @Column({nullable: true})
+    address: string
+    
+    @Column({ default: true })
+    isActive: boolean;
+}
